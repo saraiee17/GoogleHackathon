1283d23413f4f515008e60305bf8c3c739aa73c6
Aaron Mahabir <aaroncurtis32@gmail.com>
2022-12-02 02:53:38 -0500

    coffee shop page

--- src/pages/coffeeshop/coffeshop.js
+++ src/pages/coffeeshop/coffeshop.js
@@ -1,10 +1,13 @@
 import './coffeeshop.scss';
+import mainPage from '../../assets/coffeeshopIcon/Coffee_Shop_Details.png';
 
 function Coffeeshop(){
 
     return(
         <div className='coffeeContainer'>
-        
+            <img className='mainPic' src={mainPage} alt="" />
+            <button className='photos'></button>
+            <button className='more'></button>
         </div>
     )
 }
